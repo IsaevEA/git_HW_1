d957db9ae74cd3498e469e593a076d80b0d6ac85
erjanisaev42@gmail.com <erjanisaev42@gmail.com>
2024-07-18 11:46:09 +0600

    Поменяли игру, изменили цикл и дали ограниченное количесто попыток

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,8 +8,11 @@ public class Main {
         Random random = new Random();
         int num1 = random.nextInt(100)+1;
         Scanner scanner = new Scanner(System.in);
-        while (true){
-            System.out.println("Угадай число от 1 до 100");
+        System.out.println("Угадайте за 5 попыток число от 1 до 100");
+        int count = 0;
+        for (int i = 0; i < 5; i++) {
+            count++;
+            System.out.println("Попытка " + count);
             int num2 = scanner.nextInt();
             if (num2 == num1){
                 System.out.println("Игра закончена");
@@ -20,5 +23,6 @@ public class Main {
                 System.out.println("ваше число меньше чем мое");
             }
         }
+        System.out.println("Загаданное число " + num1);
     }
 }
