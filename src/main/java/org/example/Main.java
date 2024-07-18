a4b9080dea65a58d75000fa4f6615ae4123431c2
erjanisaev42@gmail.com <erjanisaev42@gmail.com>
2024-07-18 11:23:26 +0600

    перешли по ветке Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,24 @@
 package org.example;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Random random = new Random();
+        int num1 = random.nextInt(100)+1;
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            System.out.println("Угадай число от 1 до 100");
+            int num2 = scanner.nextInt();
+            if (num2 == num1){
+                System.out.println("Игра закончена");
+                break;
+            } else if (num2> num1) {
+                System.out.println("ваше число больше чем мое");
+            } else {
+                System.out.println("ваше число меньше чем мое");
+            }
+        }
     }
 }
